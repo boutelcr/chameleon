23f5cdfd1d36bd5bc5a3a2cc61ac435981550f6c
dooleys1 <dooleys1@137.112.237.93>
2021-05-13 11:51:00 -0400

    yo, window builder. Get grinding.

--- ENGD250 Ethiopian App Project/src/Window_Chameleon.java
+++ ENGD250 Ethiopian App Project/src/Window_Chameleon.java
@@ -1,5 +1,4 @@
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JButton;
@@ -9,9 +8,16 @@ import javax.swing.AbstractAction;
 import java.awt.event.ActionEvent;
 import javax.swing.Action;
 import java.awt.Color;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
 
 public class Window_Chameleon {
-
+	ImageIcon homePage = new ImageIcon("home_page_shoe.png");
 	private JFrame frame;
 	private final Action action = new SwingAction();
 
@@ -43,32 +49,37 @@ public class Window_Chameleon {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 877, 653);
+		frame.setBounds(100, 100, 942, 792);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(Color.GRAY);
-		panel.setBounds(10, 10, 843, 596);
+		panel.setBackground(Color.DARK_GRAY);;
+		panel.setBounds(10, 10, 910, 744);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
-		JCheckBox btnNewButton = new JCheckBox("New button");
-		btnNewButton.setBounds(660, 10, 173, 130);
-		panel.add(btnNewButton);
+		JButton newDesign = new JButton("New Design");
+		newDesign.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		newDesign.setBounds(377, 239, 153, 84);
+		panel.add(newDesign);
 		
-		JToggleButton btnNewButton_1 = new JToggleButton("New button");
-		btnNewButton_1.setAction(action);
-		btnNewButton_1.setBounds(660, 150, 173, 130);
-		panel.add(btnNewButton_1);
+		JButton pastDesign = new JButton("Past Design");
+		pastDesign.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		pastDesign.setBounds(377, 381, 153, 84);
+		panel.add(pastDesign);
 		
-		JButton thing = new JButton("thing");
-		thing.setBounds(660, 299, 173, 130);
-		panel.add(thing);
+		JLabel welcome = new JLabel("Welcome to Chameleon");
+		welcome.setForeground(Color.WHITE);
+		welcome.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		welcome.setBounds(227, 57, 436, 57);
+		panel.add(welcome);
 		
-		JButton btnNewButton_3 = new JButton("New button");
-		btnNewButton_3.setBounds(670, 439, 173, 130);
-		panel.add(btnNewButton_3);
+		JLabel homeShoe = new JLabel("");
+		Image img_1 = new ImageIcon(this.getClass().getResource("home_page_shoe.png")).getImage();
+		homeShoe.setIcon(new ImageIcon(img_1));
+		homeShoe.setBounds(10, 0, 885, 744);
+		panel.add(homeShoe);
 	}
 	private class SwingAction extends AbstractAction {
 		public SwingAction() {
